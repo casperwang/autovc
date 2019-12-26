c8d0cc7ed142cbad625a2f9a7a3a6871eceba451
xSeanliux <zhxnliu@gmail.com>
2019-12-26 10:51:49 +0800

    Added more stuff to train.py
    Copied some code off of conversion.py and updated train

--- train.py
+++ train.py
@@ -4,7 +4,17 @@ import torch
 import torch.optim as optim
 import model_vc as models
 import torch.functional as F
+import dataLoader as datas
 model = sy.build_model()
+
+iters_per_epoch = 100
+
+device = "cpu"
+G = Generator(32, 256, 512, 32).eval().to(device)
+
+g_checkpoint = torch.load("trainchk.ckpt", map_location = torch.device(device)) #trainchk.ckpt is the file to train
+
+G.load_state_dict(g_checkpoint["model"])
 
 class Trainer(nn.Module):
 	def __init__(self): #Build model here
@@ -37,7 +47,11 @@ trainer = Trainer()
 optimizer = optim.Adam(trainer.parameters(), lr = 0.0001) #Not sure what the parameters do, just copying it
 
 def train(epochs): #TODO once data loader is complete
+	total_it = 0
 	for epoch in range(epochs):
+		for it in range(iters_per_epoch):
+			total_it = total_it + 1
+
 		#Load data -> zero gradients -> forward + backward + optimize -> perhaps print stats?
 		
 
