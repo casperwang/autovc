1591fb59325004c10d30efebf70aac4e9c77a8c9
casperwang <casperwang2014@gmail.com>
2019-12-31 16:27:23 +0800

    update

--- data_loader/dataLoader.py
+++ data_loader/dataLoader.py
@@ -15,7 +15,7 @@ class voiceDataset(Dataset):
         item['person'] = 'p001'
         tmp = np.zeros((256), dtype='float64')
         tmp[0] = 1
-        item['style'] = torch.from_numpy(tmp[np.newaxis,:]).cpu().float()
+        item['style'] = torch.from_numpy(tmp[np.newaxis, :]).cpu().float()
         item['spectrogram'] = torch.from_numpy(self.wav_folder[index][1][np.newaxis, :, :]).cpu().float()
         # item[0] 人名 : p001(用來train的data)
         # item[1] Style : 還沒有 Style encoder
