678d5e65d8513b5ba284e440995fdec801fe324c
xSeanliux <zhxnliu@gmail.com>
2020-03-31 13:47:13 +0800

    Update

--- Demo/autovc/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AutovcConfig(AppConfig):
-    name = 'autovc'
--- train_style.py
+++
@@ -1,5 +0,0 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
-import ge2e #Loss function
